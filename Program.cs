4f066f88db3d97d5c6df86cf4e9f087d4c583484
williamkimball <36681575+williamkimball@users.noreply.github.com>
2018-08-22 14:31:54 -0500

    slight refactor

--- Program.cs
+++ Program.cs
@@ -30,7 +30,7 @@ namespace lists {
 
             Dictionary<string, string> Mars = new Dictionary<string, string> () { { "Mars Global Surveyor", "Mars" }, { "InSight", "Mars" } };
 
-            Dictionary<string, string> Jupiter = new Dictionary<string, string> () { { "Voyager 2", "Pioneer 10" }, { "Ulysses", "Jupiter" } };
+            Dictionary<string, string> Jupiter = new Dictionary<string, string> () { { "Pioneer 10", "Jupiter" }, { "Ulysses", "Jupiter" } };
 
             Dictionary<string, string> Saturn = new Dictionary<string, string> () { { "Cassini", "Saturn" }, { "Huygens", "Saturn" } };
 
@@ -57,10 +57,76 @@ namespace lists {
                             probeNameList.Add (planetProbes.Key);
                         }
                     }
-                string probeNameListString = string.Join (", ", probeNameList.ToArray ());
-                Console.WriteLine ("{0}: {1}", planet, probeNameListString);
+
+                Console.WriteLine ("{0}: {1}", planet, string.Join (", ", probeNameList));
             }
 
         }
     }
 }
+
+// using System;
+// using System.Collections.Generic;
+
+
+// namespace collections
+// {
+//     class Program
+//     {
+//         static void Main(string[] args)
+//         {
+//             // List of planets
+//             List<string> planetList = new List<string>() { "Mercury", "Mars" };
+
+//             // Empty list of probes
+//             List<Dictionary<string, string>> probes = new List<Dictionary<string, string>>();
+
+//             // Create probe
+//             Dictionary<string, string> viking = new Dictionary<string, string>() {
+//                 { "Mars", "Viking" }
+//             };
+
+//             // Create probe
+//             Dictionary<string, string> opportunity = new Dictionary<string, string>() {
+//                 { "Mars", "Opportunity" }
+//             };
+
+//             // Create probe
+//             Dictionary<string, string> mariner10 = new Dictionary<string, string>() {
+//                 { "Mercury", "Mariner 10" }
+//             };
+
+//             // Add probes to the List
+//             probes.Add(viking);
+//             probes.Add(opportunity);
+//             probes.Add(mariner10);
+
+//             /*
+//                 The planet name itself is essentially a foreign key in the probes
+//                 list back to the planet list.
+//                 Iterate over all the planets.
+//              */
+//             foreach (string planet in planetList)
+//             {
+//                 // Build an initial list of string to hold the matching probe names
+//                 List<string> visitedProbes = new List<string>();
+
+//                 // Iterate over all the probes
+//                 foreach (Dictionary<string, string> probe in probes)
+//                 {
+//                     /*
+//                         Does the current planet name in the dictionary match
+//                         the name of the planet in the planet list
+//                      */
+//                     if (probe.ContainsKey(planet)) {
+//                         visitedProbes.Add(probe[planet]);
+//                     }
+
+//                 }
+
+//                 // Output the name of the planet and comma separated list of matching probes
+//                 Console.WriteLine($"{planet}: {String.Join(", ", visitedProbes)}");
+//             }
+//         }
+//     }
+// }
